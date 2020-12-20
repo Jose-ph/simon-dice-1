016ee0042d0b58365430570845841d3edde0635b
Jose <jose_esteban_basualdo@hotmail.com>
2020-12-19 22:17:53 -0300

    Define elementos del juego
    partes básicas para el funcionamiento del juego.

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
+//Elementos del juego
 const $tablero = document.querySelector('#tablero');
+
+const $botonJugar = document.querySelector('#boton-jugar')
+
+const $botonResetear = document.querySelector('#boton-resetear');
+
+let $estado = document.querySelector('#estado');
 let $cuadroRojo = document.querySelector('#rojo');
+
 let $cuadroVerde = document.querySelector('#verde');
+
 let $cuadroAzul = document.querySelector('#azul');
+
 let $cuadroAmarillo = document.querySelector('#amarillo');
+
+
+$botonJugar.onclick = function(){
+    console.log("QUE EMPIECE EL JUEGO");
+}
+
+$botonResetear.onclick = function(){
+
+    console.log("TOCASTE RESETEAR");
+}
+
+$estado.innerText = "El juego está en preparación";
