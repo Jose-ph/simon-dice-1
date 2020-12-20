b81ea453b6868e2b5fa49a3cc3b7a27469ac07ff
Jose <jose_esteban_basualdo@hotmail.com>
2020-12-19 22:10:02 -0300

    Agrega botones

---
+++ main.js
@@ -0,0 +1,5 @@
+const $tablero = document.querySelector('#tablero');
+let $cuadroRojo = document.querySelector('#rojo');
+let $cuadroVerde = document.querySelector('#verde');
+let $cuadroAzul = document.querySelector('#azul');
+let $cuadroAmarillo = document.querySelector('#amarillo');
