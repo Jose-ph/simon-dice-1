322646a1efc33a784d5957d063b0bee9b364e808
Jose <jose_esteban_basualdo@hotmail.com>
2021-01-01 13:05:59 -0300

    agrega manejarIluminacionCuadros
    Los cuadros se encienden luego de un segundo y se apagan al siguiente segundo

--- main.js
+++ main.js
@@ -1,179 +1,140 @@
-//Elementos del juego
-const $tablero = document.querySelector('#tablero');
-
-const $botonJugar = document.querySelector('#boton-jugar')
-
-const $botonResetear = document.querySelector('#boton-resetear');
-
+const $tablero= document.querySelector('#tablero');
 let $estado = document.querySelector('#estado');
-
+const $botonStart= document.querySelector('#boton-start');
+let $cuadros = document.querySelectorAll(".cuadro");
 const $cuadroRojo = document.querySelector('#rojo');
-
+const $cuadroAzul= document.querySelector('#azul');
 const $cuadroVerde = document.querySelector('#verde');
-
-const $cuadroAzul = document.querySelector('#azul');
-
 const $cuadroAmarillo = document.querySelector('#amarillo');
 
-// variables para jugar
+let nivel;
 
-//let secuenciaMaquina =[$cuadroRojo , $cuadroAzul , $cuadroVerde,$cuadroAmarillo ];    //cambiar a ILUMINARJUGADA
+let jugadaMaquina = [];
 
-//let secuenciaJugador =[];
+let jugadaUsuario = []; 
 
-//let jugadaMaquina = [];
+let indicadorSecuencia;
 
-//let turnoMaquina = true;
+$estado.innerText = "Toca START para empezar el juego";
 
 
-$botonJugar.onclick = function(){
+
+$botonStart.onclick = function (){
+
+    let nivel =2;
+
+    let jugadaMaquina =[];
+    
+    let jugadaUsuario = []; 
+    
+    let indicadorSecuencia;
+    
+    $estado.innerText = "Estas en el nivel " + nivel;    
+
+    jugar(jugadaMaquina);
+
 
     
-
-
-} 
-   
-
-
-
-
-function crearJugadaMaquina(jugadaMaquina){
-
-    let $jugadaMaquina = jugadaMaquina;
-
-    for( let i=0 ; i < 5 ; i++){ // numero de ROUNDS para ganar
-    
-    
-        $jugadaMaquina.push(obtenerCuadroAleatorio());
-         
-      }
-
-      return $jugadaMaquina;
-
 }
 
 
+    function jugar(jugadaMaquina , jugadaUsuario , indicadorSecuencia , nivel){
+        
+        
 
 
+        agregarCuadroAleatorio(jugadaMaquina);
 
+        console.log(jugadaMaquina);
 
+         setTimeout(function(){
+             manejarIluminacionCuadros(jugadaMaquina);
 
+            
+         }, 1000)// deberia tardar 1 esgundo en prender
 
+       
+    }
 
+    function agregarCuadroAleatorio (jugadaMaquina){
 
 
 
+        let $jugadaMaquina = jugadaMaquina;
 
+         let $cuadros = document.querySelectorAll(".cuadro");
 
+        let indiceAleatorio = Math.round(Math.random() * 3); 
 
+        let cuadroAleatorio = $cuadros[indiceAleatorio];
+       
 
-function obtenerCuadroAleatorio(){
+            $jugadaMaquina.push(cuadroAleatorio);
 
-    let cuadros = document.querySelectorAll('.cuadro');
-    let cuadroElegido ;
-   let indiceAleatorio = Math.round(Math.random() *3) // esto pone el rango para que salga el índice
-
-
-
-
-        cuadroElegido = cuadros[indiceAleatorio];
-
-
-   
-     
-
-     return cuadroElegido;
-}
-
-
-
-function encenderCuadroRojo(evento){
-
-    let $cuadroRojo = document.querySelector('#rojo');
-
-    $cuadroRojo.style.backgroundColor = "#f95b5b"
-
-
-}
-
-function apagarCuadroRojo(evento){
-
-    let $cuadroRojo = document.querySelector('#rojo');
-
-    $cuadroRojo.style.backgroundColor = "red";
-}
-
-function encenderCuadroAzul (evento){
-
-    let $cuadroAzul = document.querySelector('#azul');
-
-    $cuadroAzul.style.backgroundColor = "#7779f9";
-
-
-}
-
-function apagarCuadroAzul (evento){
-
-    let $cuadroAzul = document.querySelector('#azul');
-
-    $cuadroAzul.style.backgroundColor = "blue";
-
-
-}
-
-function encenderCuadroVerde (evento){
-
-    let $cuadroVerde = document.querySelector('#verde');
-
-    $cuadroVerde.style.backgroundColor = "#57d16d";
-
-
-}
-
-function apagarCuadroVerde (evento){
-
-    let $cuadroVerde = document.querySelector('#verde');
-
-    $cuadroVerde.style.backgroundColor = "green";
-
-
-}
-
-function encenderCuadroAmarillo (evento){
-
-    let $cuadroAmarillo = document.querySelector('#amarillo');
-
-    $cuadroAmarillo.style.backgroundColor = " #fcfc7c ";
-    
-
-
-}
-
-function apagarCuadroAmarillo (evento){
-
-    let $cuadroAmarillo = document.querySelector('#amarillo');
-
-    $cuadroAmarillo.style.backgroundColor = "yellow";
-
-
-}
-
-
-
-
+            
+        return $jugadaMaquina;
 
         
 
 
 
+    }
 
+    function manejarIluminacionCuadros(jugada){ //toma la del usuario o la de la maquina
+
+        let $jugada = jugada;
+
+        $jugada.forEach(cuadro => { //enciende los cuadros y apaga
+           
+            if (cuadro === $cuadroRojo){
+
+                $cuadroRojo.style.background = "#fc8787";
+
+                setTimeout (function(){
+
+                    $cuadroRojo.style.background = "red";
+
+                },800);
+            }
     
-
-
-
-
+            if(cuadro === $cuadroAzul){
     
+                $cuadroAzul.style.background = " #717dfa ";
+                setTimeout (function(){
+
+                    $cuadroAzul.style.background = "blue";
+
+                },800);
 
 
+            }
+            
+            if( cuadro === $cuadroVerde){
+    
+    
+                $cuadroVerde.style.background = " #62f64e ";
+
+                setTimeout (function(){
+
+                    $cuadroVerde.style.background = "green";
+
+                },800);
+            }
+    
+            if(cuadro === $cuadroAmarillo){
+    
+                $cuadroAmarillo.style.background = "#fafa8e ";
+
+                setTimeout (function(){
+
+                    $cuadroAmarillo.style.background = "yellow";
+
+                },800);
+            }
+      
+        });
+
+        
 
 
+    }
